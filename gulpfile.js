b9aa7905fb91a9f1cd891fd98db4c3ad2852b667
Ross <rdegnen@gmail.com>
2015-07-15 11:50:52 -0400

    added test task to gulp file

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+
+// Run 'npm test' or 'gulp test' to run test suite
+gulp.task('test', function() {
+  return gulp.src(['./server/tests/specs/*.js'], {read: false})
+    .pipe(mocha({reporter: 'spec'}))
+    .on('error', guitl.log)
+    .once('end', function() {
+      process.exit();
+    });
+});
