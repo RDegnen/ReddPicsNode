3040dd7a5a4513a1c09eb4297eb48a9d43dfeb11
Ross <rdegnen@gmail.com>
2015-07-15 16:36:27 -0400

    wrote test for homePage call

--- server/tests/specs/imgurApiSpec.js
+++ server/tests/specs/imgurApiSpec.js
@@ -1,7 +1,22 @@
 'use strict';
 
 var chai = require('chai');
+var request = require('superagent');
+var url = 'http://localhost:3000';
 
 describe('Imgur API', function() {
-
+  describe('API call', function() {
+    it('should respond with json', function(done) {
+      request.get(url + '/imgur/index')
+        .end(function(err, res) {
+          if (err) {
+            console.log(err);
+          }
+          chai.expect(res.body).to.exist;
+          chai.expect(res.statusCode).to.equal(200);
+          chai.expect(res).to.be.json;
+          done();
+        });
+    });
+  });
 });
