c39e9711af7357c559010304b95164a9848914ef
Ross <rdegnen@gmail.com>
2015-07-15 12:39:54 -0400

    refactored a bit of app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var http = require('http');
 var express = require('express');
 
 var app = express();
-
+var port = process.env.PORT || 3000;
 // Express config
 require('./server/config/express');
 
-var server = http.createServer(app);
+app.use('/', require('./server/routes'));
 
-server.listen(3000);
+http.createServer(app)
+  .listen((port), function() {
+    console.log('Server listening on port ' + port);
+});
