706e836250f65a920791caa62fb62581e8da9ac6
Ross <rdegnen@gmail.com>
2015-07-15 13:58:22 -0400

    added getHomePage function to controller, have yet to test

--- server/api/controllers/imgur.js
+++ server/api/controllers/imgur.js
@@ -1,7 +1,22 @@
 'use strict';
 
-var imgurApi = {
+var secrets = require('../../config/secrets.js');
+var request = require('superagent');
 
+var imgurApi = {
+  getHomePage: function(cb) {
+    var subredditArray = ['pics', 'historyporn', 'food', 'aww', 'wallpapers', 'carporn', 'space', 'sports', 'art', 'beerporn'];
+    var randomSubreddit = subredditArray[Math.floor(Math.random() * subredditArray.length)];
+
+    request
+      .get("https://api.imgur.com/3/gallery/r/" + randomSubreddit)
+      .set({
+        Authorization: secrets.IMGUR_CLIENT_ID
+      })
+      .end(function(err, doc) {
+        cb(err, doc);
+      });
+  }
 };
 
 module.exports = imgurApi;
