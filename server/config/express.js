613746c0d4fa4d9102af10114fe364ad262baca0
Ross <rdegnen@gmail.com>
2015-07-16 12:00:00 -0400

    added logger

--- server/config/express.js
+++ server/config/express.js
@@ -5,9 +5,11 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var fs = require('fs');
+var logger = require('morgan');
 
 var config = function(app, express) {
 
+  app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 
