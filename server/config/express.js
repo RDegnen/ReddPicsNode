ec5cfe0e69f0cd13698f49f66ed8b2bdd8651047
Ross <rdegnen@gmail.com>
2015-08-03 13:47:25 -0400

    dont think cors is the problem

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,7 @@ var errorhandler = require('errorhandler');
 
 var config = function(app, express) {
 
-  app.all('/' ,function(req, res, next) {
+  app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
