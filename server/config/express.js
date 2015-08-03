c176c97534ff4289607b2a867a1653e02c2c8a5b
Ross <rdegnen@gmail.com>
2015-08-03 11:11:09 -0400

    adding more cors

--- server/config/express.js
+++ server/config/express.js
@@ -10,6 +10,7 @@ var errorhandler = require('errorhandler');
 
 var config = function(app, express) {
 
+  app.use(allowCrossDomain);
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +25,7 @@ var config = function(app, express) {
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
